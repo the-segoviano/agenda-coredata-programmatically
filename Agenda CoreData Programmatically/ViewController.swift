ebd4b3d8e36693da4486267359f48d0603b4a21b
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-21 23:57:35 -0600

    Initial Commit

---
+++ Agenda CoreData Programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Agenda CoreData Programmatically
+//
+//  Created by Luis Segoviano on 21/01/20.
+//  Copyright © 2020 The Segoviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

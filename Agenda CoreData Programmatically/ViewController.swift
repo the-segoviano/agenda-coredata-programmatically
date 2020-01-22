6eb881be5ed65aa3c531f0bcbc989f556649fe22
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-22 00:42:11 -0600

    Functions for integration UITableView and user's items with CoreData

--- Agenda CoreData Programmatically/ViewController.swift
+++ Agenda CoreData Programmatically/ViewController.swift
@@ -7,29 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
 
-    var people: [String] = []
+    var people: [NSManagedObject] = []
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        return managedContext
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "List Names"
-        self.tableView.dataSource = self
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        title = "Names"
         self.view.backgroundColor = .white
-        let addItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.addName))
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.reloadData()
+        let addItem = UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                                      action: #selector(self.addName))
         self.navigationItem.rightBarButtonItem = addItem
+        let clearDataItem = UIBarButtonItem(title: "Clear", style: .plain,
+                                            target: self, action: #selector(self.clearData))
+        self.navigationItem.leftBarButtonItem = clearDataItem
     }
     
-    @objc func addName(){
+    @objc func addName() {
         let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
             guard let textField = alert.textFields?.first,
                 let nameToSave = textField.text else {
                     return
             }
-            self.people.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
@@ -39,15 +52,77 @@ class ViewController: UITableViewController {
         present(alert, animated: true)
     }
     
+    // Mark: UITableView's Delegate and Datasource
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people.count
+        let fetchRequest   = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        do {
+            people = try managedContext.fetch(fetchRequest)
+            return people.count
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = people[indexPath.row]
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    // Mark: Coredata
+    
+    func save(name: String) {
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        person.setValue(name, forKeyPath: "name")
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    @objc func clearData() {
+        let spinner = UIActivityIndicatorView(style: .gray)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        view.addSubview(spinner)
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        do {
+            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+            let persons = try(managedContext.fetch(fetchRequest))
+            for person in persons {
+                managedContext.delete(person)
+            }
+            try(managedContext.save())
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // simulate something is happening by 1 sec.
+                self.tableView.reloadData()
+                spinner.stopAnimating()
+            }
+        } catch let err {
+            print(" err ", err)
+        }
+    }
+    
+    func fetchNameFromDisk() {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
 
 }

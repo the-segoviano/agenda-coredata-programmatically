c082146e2c04c771c5d0f52e982db0c289bdf369
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-22 00:10:36 -0600

    Deleting StoryBoard and adding item in NavigationBar

--- Agenda CoreData Programmatically/ViewController.swift
+++ Agenda CoreData Programmatically/ViewController.swift
@@ -10,21 +10,42 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var people: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "List Names"
         self.tableView.dataSource = self
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.view.backgroundColor = .white
+        let addItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.addName))
+        self.navigationItem.rightBarButtonItem = addItem
+    }
+    
+    @objc func addName(){
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            guard let textField = alert.textFields?.first,
+                let nameToSave = textField.text else {
+                    return
+            }
+            self.people.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return people.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.text = people[indexPath.row]
         return cell
     }
     
